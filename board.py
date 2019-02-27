1e592be5c261f1d5ad01720882fcd228a214eb5b
taylor1355 <hearntaylor@gmail.com>
2019-02-26 23:27:29 -0500

    fixed bug

--- board.py
+++ board.py
@@ -9,30 +9,31 @@ class Board:
         if board is None:
             self.board = np.zeros((length, length), dtype=int)
 
-        self.groups = np.zeros((length, length, length), dtype=int) # group, row, col; masks for where groups are
-
         # as a simplifying assumption (will change later), assume length is a perfect square
         group_length = int(math.sqrt(length))
         if length <= 0 or group_length ** 2 != length:
             raise ValueError('Length should be a positive perfect square')
 
+        self.groups = np.ones((length, length, length), dtype=int) # group, row, col; masks for where groups are
         for r in range(group_length):
             for c in range(group_length):
+                group_num = r * group_length + c
                 board_r = r * group_length
                 board_c = c * group_length
-                self.groups[r * c, board_r : board_r + group_length, board_c : board_c + group_length] = 1
+                self.groups[group_num, board_r : board_r + group_length, board_c : board_c + group_length] = 0
 
-        self.group_index = np.argmax(self.groups, axis=0) # group index at every position
+        self.group_index = np.argmin(self.groups, axis=0) # group index at every position
 
-        self.possible = np.zeros((length, length, length), dtype=int) # value, row, col; mask for where new values can be placed
+        self.possible = np.ones((length, length, length), dtype=int) # value, row, col; mask for where new values can be placed
         self.calculate_possible()
 
-    # todo: this may be much more efficient using hashmapsrather than np arrays, certainly for large lengths
+    # todo: this may be much more efficient using hashmaps rather than np arrays, certainly for large lengths, also by keeping track of non-empty tiles
     def calculate_possible(self):
         for r in range(self.length):
             for c in range(self.length):
-                val = self.board[r, c]
-                if val != 0:
-                    self.possible[val - 1, r, :] = 0
-                    self.possible[val - 1, :, c] = 0
-                    self.possible[val - 1][self.groups[val]] = 0
+                val_index = self.board[r, c] - 1
+                if val_index != -1:
+                    self.possible[val_index, r, :] = 0
+                    self.possible[val_index, :, c] = 0
+                    group_index = self.group_index[r, c]
+                    self.possible[val_index] *= self.groups[group_index]
