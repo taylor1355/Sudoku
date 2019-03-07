af9dab89a80e14dfe0cc46f0f578ebedf1ef24cc
taylor1355 <hearntaylor@gmail.com>
2019-03-07 11:43:30 -0500

    finalized basic board logic and implemented CSP solver

--- board.py
+++ board.py
@@ -1,13 +1,9 @@
 import numpy as np
 import math
 
-class Board:
-    def __init__(self, length, board=None):
+class BoardSpec:
+    def __init__(self, length):
         self.length = length
-
-        self.board = board # row, col; current values
-        if board is None:
-            self.board = np.zeros((length, length), dtype=int)
 
         # as a simplifying assumption (will change later), assume length is a perfect square
         group_length = int(math.sqrt(length))
@@ -24,16 +20,37 @@ class Board:
 
         self.group_index = np.argmin(self.groups, axis=0) # group index at every position
 
-        self.possible = np.ones((length, length, length), dtype=int) # value, row, col; mask for where new values can be placed
-        self.calculate_possible()
+class Board:
+    def __init__(self, spec, to_copy=None):
+        self.spec = spec
+
+        if to_copy is None:
+            self.current = np.zeros((spec.length, spec.length), dtype=int) # row, col; current values
+            self.possible = np.ones((spec.length, spec.length, spec.length), dtype=int) # value, row, col; mask for where new values can be placed
+            self.calculate_possible()
+        else:
+            self.current = np.copy(to_copy.current)
+            self.possible = np.copy(to_copy.possible)
+
+    def mark(self, value, row, col):
+        self.current[row, col] = value
+        self.update_possible(row, col)
 
     # todo: this may be much more efficient using hashmaps rather than np arrays, certainly for large lengths, also by keeping track of non-empty tiles
     def calculate_possible(self):
-        for r in range(self.length):
-            for c in range(self.length):
-                val_index = self.board[r, c] - 1
-                if val_index != -1:
-                    self.possible[val_index, r, :] = 0
-                    self.possible[val_index, :, c] = 0
-                    group_index = self.group_index[r, c]
-                    self.possible[val_index] *= self.groups[group_index]
+        for r in range(self.spec.length):
+            for c in range(self.spec.length):
+                self.update_possible(r, c)
+
+    def update_possible(self, row, col):
+        val_index = self.current[row, col] - 1
+        if val_index != -1:
+            self.possible[val_index, row, :] = 0
+            self.possible[val_index, :, col] = 0
+            self.possible[:, row, col] = 0
+            group_index = self.spec.group_index[row, col]
+            self.possible[val_index] *= self.spec.groups[group_index]
+            self.possible[val_index, row, col] = 1
+
+    def copy(self):
+        return Board(self.spec, to_copy=self)
---
+++ solver.py
@@ -0,0 +1,27 @@
+import numpy as np
+import math
+
+# using forward checking
+def solve(board):
+    num_possibilities = np.sum(board.possible, axis=0)
+    flat_indices = np.argsort(num_possibilities, axis=None)
+
+    # print(board.current)
+    # print(num_possibilities)
+
+    length = board.spec.length
+    for i in range(len(flat_indices)):
+        index = flat_indices[i]
+        if num_possibilities.flat[index] == 0: # backtrack
+            return None
+        elif board.current.flat[index] == 0: # variable not already assigned
+            for value in range(1, length + 1):
+                if board.possible[value - 1].flat[index]:
+                    row, col = index // length, index % length
+                    board_copy = board.copy()
+                    board_copy.mark(value, row, col)
+                    solution = solve(board_copy)
+                    if solution is not None:
+                        return solution
+            return None
+    return board
